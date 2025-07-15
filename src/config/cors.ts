505fb6fbc23cfa4c3a64b75164a35c4fb9a95da0
Yostin Manuel Ramos Pinto <ramosyostin2003@gmail.com>
2025-07-15 00:50:02 -0600

    cors actualziado

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,20 @@
-import {CorsOptions} from 'cors'
+import { CorsOptions } from 'cors';
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL || 'https://tshirt-ymrp-store.netlify.app',
+  'http://localhost:5173', // puerto local para desarrollo
+];
 
 export const corsConfig: CorsOptions = {
-    //origin: quien esta tratando de conectarse
-    //callback: 
-    origin: function(origin, callback) {
-        if(origin === process.env.FRONTEND_URL){
-            //null: ningun error
-            //true: permitir conexion
-            callback(null,true)
-        }else{
-            callback(new Error('Error de CORS'))
-        }
+  origin: function(origin, callback) {
+    // Permite si no hay origin (ej: backend a backend o Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
     }
-}
+  },
+  credentials: true,
+};
